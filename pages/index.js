e08be36cabe09d1910ea63a540f08d27d88f43b6
Stanoid <mujahidwadmohammad@gmail.com>
2021-12-20 12:21:52 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           to <a href="https://nextjs.org">Next.js!</a>
+          
         </h1>
 
         <p className={styles.description}>
